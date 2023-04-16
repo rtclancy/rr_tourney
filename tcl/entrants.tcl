6e71416265eddc4b0aea07c369ba102769ffff32
Bob Clancy <rtclancy@comcast.net>
2023-04-16 11:56:33 -0400

    first checkin

---
+++ tcl/entrants.tcl
@@ -0,0 +1,9 @@
+set entrants_list {
+    {{Mike}     {Mikey}  	}
+    {{Dennis}   {GrandPa}	}      
+    {{Bob}      {Sandbagger Bob}}
+    {{Joe}      {Joey 3Wood}    }
+    {{Marshall} {MP3}           }
+    {{Kenny}    {D-Day Kenny}   }
+};
+
